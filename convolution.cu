c8ee66ce24de6c1d6709aaaa1235359fd6ccfefe
Karl Marrett <kdmarrett@gmail.com>
2020-04-26 20:17:07 -0700

    first working parallelized

--- convolution.cu
+++ convolution.cu
@@ -49,7 +49,6 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 #define NYSCL (Ny/Sy)
 #define NXSCL (Nx/Sx)
 
-#define SYNAPSE_SIZE (1L*Ky*Kx*Nn*Ni)
 const int output_neurons = 64;
 
 void fill(VTYPE *arr, const uint64_t size) {
@@ -117,84 +116,72 @@ std::pair<int,int> convolution_layer_blocked(
 
 __global__
 void convolution_layer(VTYPE * synapse, VTYPE* neuron_i, VTYPE* neuron_n, int Nx,
-                       int Ny, int Kx, int Ky, int Ni, int Nn) {
+                       int Ny, int Kx, int Ky, int Ni, int Nn, int B) {
   // VTYPE sum[Nn]={0};
   VTYPE sum[output_neurons];
 
   // — Original code — (excluding nn, ii loops)
   int yout = 0;
-  for (int y = 0; y < Ny; y += Sy) { // tiling for y;
-    int xout = 0;
-    for (int x = 0; x < Nx; x += Sx) { // tiling for x;
-      for (int nn = 0; nn < Nn; nn += Tn) {
-	// reset sum for this window slide area
-        for (int n = nn; n < nn + Tn; n++) {
-          sum[n]=0;
-        }
+  for (int b=0; b < B; b++) { 
+    for (int y = 0; y < Ny; y += Sy) { // tiling for y;
+      int xout = 0;
+      for (int x = 0; x < Nx; x += Sx) { // tiling for x;
+	for (int nn = 0; nn < Nn; nn += Tn) {
+	  // reset sum for this window slide area
+	  for (int n = nn; n < nn + Tn; n++) {
+	    sum[n]=0;
+	  }
 
-        // sliding window;
-        for (int ky = 0; ky < Ky; ky++)
-          for (int kx = 0; kx < Kx; kx++)
-            for (int n = nn; n < nn + Tn; n++)
-              for (int i = 0; i < Ni; i++) {
-		// VTYPE (&synapse)[Ky][Kx][Nn][Ni], 
-                VTYPE sv = synapse[ky * (Kx * Nn * Ni) + kx * (Nn * Ni) + n * Ni + i];
-                  //            VTYPE (&neuron_i)[NYPAD][NXPAD][Ni], 
-                VTYPE nv = neuron_i[(ky + y) * NXPAD * Ni + (kx + x) * Ni + i];
-                // VTYPE nv = neuron_i[ky + y][kx + x][i];
-                sum[n]+=sv*nv;
-              }
-        for (int n = nn; n < nn + Tn; n++) {
-	  // VTYPE (&neuron_n)[NYSCL][NXSCL][Nn]) {
-          neuron_n[yout * (NXSCL * Nn) + xout * Nn + n] = sum[n] > 0 ? sum[n] : sum[n]/4;
-          // neuron_n[yout][xout][n] = transfer(sum[n]);
-        }
+	  // sliding window;
+	  for (int ky = 0; ky < Ky; ky++)
+	    for (int kx = 0; kx < Kx; kx++)
+	      for (int n = nn; n < nn + Tn; n++)
+		for (int i = 0; i < Ni; i++) {
+		  // VTYPE (&synapse)[B][Ky][Kx][Nn][Ni], 
+		  VTYPE sv = synapse[b * (Ky * Kx * Nn * Ni) + ky * (Kx * Nn * Ni) + kx * (Nn * Ni) + n * Ni + i];
+		  // VTYPE (&neuron_i)[B][NYPAD][NXPAD][Ni], 
+		  VTYPE nv = neuron_i[b * (NYPAD * NXPAD * Ni) + (ky + y) * NXPAD * Ni + (kx + x) * Ni + i];
+		  sum[n]+=sv*nv;
+		}
+	  for (int n = nn; n < nn + Tn; n++) {
+	    // VTYPE (&neuron_n)[B][NYSCL][NXSCL][Nn]) {
+	    neuron_n[b * (NYSCL * NXSCL * Nn) + yout * (NXSCL * Nn) + xout * Nn + n] = sum[n] > 0 ? sum[n] : sum[n]/4;
+	    // neuron_n[yout][xout][n] = transfer(sum[n]);
+	  }
+	}
+	xout++; 
       }
-      xout++; 
+      yout++;
     }
-    yout++;
   }
 }
 
 __global__
-void convolution_layer_parallel(VTYPE * synapse, VTYPE* neuron_i, VTYPE* neuron_n, int Nx,
-                       int Ny, int Kx, int Ky, int Ni, int Nn) {
-  // VTYPE sum[Nn]={0};
-  VTYPE sum[output_neurons];
+void convolution_layer_parallel(VTYPE * synapse, VTYPE* neuron_i,
+VTYPE* neuron_n, int Nx, int Ny, int Kx, int Ky, int Ni, int Nn,
+int B, int tile_num_width) {
+  VTYPE sum = 0;
+  int b, y, x, n;
+  b = blockIdx.x;
+  n = blockIdx.y;
+  y = blockIdx.z / tile_num_width + threadIdx.y;
+  x = blockIdx.z % tile_num_width + threadIdx.x;
 
-  // — Original code — (excluding nn, ii loops)
-  int yout = 0;
-  for (int y = 0; y < Ny; y += Sy) { // tiling for y;
-    int xout = 0;
-    for (int x = 0; x < Nx; x += Sx) { // tiling for x;
-      for (int nn = 0; nn < Nn; nn += Tn) {
-	// reset sum for this window slide area
-        for (int n = nn; n < nn + Tn; n++) {
-          sum[n]=0;
-        }
-
-        // sliding window;
-        for (int ky = 0; ky < Ky; ky++)
-          for (int kx = 0; kx < Kx; kx++)
-            for (int n = nn; n < nn + Tn; n++)
-              for (int i = 0; i < Ni; i++) {
-		// VTYPE (&synapse)[Ky][Kx][Nn][Ni], 
-                VTYPE sv = synapse[ky * (Kx * Nn * Ni) + kx * (Nn * Ni) + n * Ni + i];
-                  //            VTYPE (&neuron_i)[NYPAD][NXPAD][Ni], 
-                VTYPE nv = neuron_i[(ky + y) * NXPAD * Ni + (kx + x) * Ni + i];
-                // VTYPE nv = neuron_i[ky + y][kx + x][i];
-                sum[n]+=sv*nv;
-              }
-        for (int n = nn; n < nn + Tn; n++) {
-	  // VTYPE (&neuron_n)[NYSCL][NXSCL][Nn]) {
-          neuron_n[yout * (NXSCL * Nn) + xout * Nn + n] = sum[n] > 0 ? sum[n] : sum[n]/4;
-          // neuron_n[yout][xout][n] = transfer(sum[n]);
-        }
+  // sliding window;
+  for (int ky = 0; ky < Ky; ky++) {
+    for (int kx = 0; kx < Kx; kx++) {
+      for (int i = 0; i < Ni; i++) {
+	// VTYPE (&synapse)[B][Ky][Kx][Nn][Ni]
+	VTYPE sv = synapse[b * (Ky * Kx * Nn * Ni) + ky * (Kx * Nn * Ni) + kx * (Nn * Ni) + n * Ni + i];
+	// VTYPE (&neuron_i)[B][NYPAD][NXPAD][Ni]
+	VTYPE nv = neuron_i[b * (NYPAD * NXPAD * Ni) + (ky + y) * NXPAD * Ni + (kx + x) * Ni + i];
+	sum +=sv*nv;
       }
-      xout++; 
     }
-    yout++;
   }
+  // VTYPE (&neuron_n)[B][NYSCL][NXSCL][Nn]) {
+  neuron_n[b * (NYSCL * NXSCL * Nn) + y * (NXSCL * Nn) + x * Nn + n] = sum > 0 ? sum : sum/4;
+  // neuron_n[yout][xout][n] = transfer(sum[n]);
 }
 
 int main(const int argc, const char** argv) {
@@ -207,13 +194,14 @@ int main(const int argc, const char** argv) {
   int Nn = atoi(argv[6]);
   printf("Nx:%d Ny:%d Kx:%d Ky:%d Ni:%d Nn:%d\n", Nx, Ny, Kx, Ky, Ni, Nn);
 
-  
   // SYNAPSE_SIZE can safely fit in an int for both toy example convolution sizes
   // neuron_i_size also fits in an int
   // the rest are smaller
-  int neuron_i_size = NYPAD * NXPAD * Ni;
-  int neuron_n_size = NYSCL * NXSCL * Nn;
-  int neuron_n2_size = neuron_n_size;
+  uint32_t B = 8;
+  uint32_t SYNAPSE_SIZE = static_cast<uint32_t>(Ky) *Kx*Nn*Ni;
+  uint32_t neuron_i_size = B * NYPAD * NXPAD * Ni;
+  uint32_t neuron_n_size = B * NYSCL * NXSCL * Nn;
+  uint32_t neuron_n2_size = B * neuron_n_size;
   
   cout << "synapse size: " << SYNAPSE_SIZE << '\n';
   cout << "neuron i size: " << neuron_i_size << '\n';
@@ -240,7 +228,7 @@ int main(const int argc, const char** argv) {
 
   //Simple Version
   begin_roi();
-  convolution_layer<<<1,1>>>(synapse, neuron_i, neuron_n, Nx, Ny, Kx, Ky, Ni, Nn);
+  convolution_layer<<<1,1>>>(synapse, neuron_i, neuron_n, Nx, Ny, Kx, Ky, Ni, Nn, B);
   end_roi();
 
   cout << "simple version complete!\n";  
@@ -249,22 +237,16 @@ int main(const int argc, const char** argv) {
 
   //Simple Version
   begin_roi();
-  convolution_layer_parallel<<<1,1>>>(synapse, neuron_i, neuron_n2, Nx, Ny, Kx, Ky, Ni, Nn);
+  int tile_num_width = NXSCL / Tn;
+  int tile_num_height = NYSCL / Tn;
+  int total_tile_num = tile_num_width * tile_num_height;
+  dim3 blockDim(Tn, Tn, 1);
+  dim3 gridDim(B, Nn, total_tile_num);
+  convolution_layer_parallel<<<gridDim, blockDim>>>(synapse, neuron_i, neuron_n2, Nx, Ny, Kx, Ky, Ni, Nn, B, tile_num_width);
   end_roi();
 
   cout << "parallel version complete!\n";  
 
-
-  /*
-  //Blocked Version
-  begin_roi();
-  convolution_layer_blocked(*synapse,*neuron_i,*neuron_n2);
-  end_roi();
-
-
-  cout << "blocked computation complete!\n";  
-
-  */
   compare(neuron_n, neuron_n2,NYSCL*NXSCL*Nn);
 
   cout << "done\n";
